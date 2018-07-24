4bb5d550a005ac5180c2bb51e02c23e06a0efe61
Harini <k.aravindshankar@gmail.com>
2018-07-24 22:41:41 +0530

    added sum funtion

--- welcome.c
+++ welcome.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
+#include<string.h>
+int sum(int,int);
 int main()
 {
 int a,b;
-printf("Enter the number ");
+printf("Enter the number a ");
 scanf("%d",&a);
-b=++a;
-printf("\nNext number is:%d",b);
+printf("\Enter the numbr b ");
+scanf("%d",&b);
+printf("\nThe sum of the two numbres is:%d",sum(a,b));
 return 0;
+}
+int sum(int x, int y)
+{
+	int z;
+	z=x+y;
+	return z;
 }
 
